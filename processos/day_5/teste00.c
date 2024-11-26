8dfe60ce571568ad046c2eaef04454b3dc34e05b
cesaltinofelix <cesaltinofelix2000@gmail.com>
2024-11-26 13:33:18 +0000

    Style: Mensagens Mais Descritivas refactor: Tratamento de Terminação Anormal

--- processos/day_5/teste00.c
+++ processos/day_5/teste00.c
@@ -17,17 +17,24 @@ int main(void) {
     }
 
     if(pid == 0) {
-
+        printf("Filho PID = %d: substituindo pelo programa '/bin/ls'...\n", getpid());
         execlp("/bin/ls", "ls", "-l", (char *)NULL);
-
+        // Caso execlp falhe:
+        perror("Erro ao executar execlp");
+        return (2);
     }else {
         int status;
         int terminated_pid;
 
         while((terminated_pid = wait(&status)) > 0)
         {
-            if(WIFEXITED(status))
-                printf("O processo filho com PID = %d terminou com estatus %d\n", terminated_pid, WEXITSTATUS(status));
+             if (WIFEXITED(status)) {
+                printf("Pai: o processo filho com PID = %d terminou com status = %d\n", 
+                       terminated_pid, WEXITSTATUS(status));
+            } else if (WIFSIGNALED(status)) {
+                printf("Pai: o processo filho com PID = %d foi encerrado pelo sinal = %d\n", 
+                       terminated_pid, WTERMSIG(status));
+            }
         }
     }
     return (0);
