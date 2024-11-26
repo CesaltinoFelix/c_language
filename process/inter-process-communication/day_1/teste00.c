c31d8a02ae09e60187892d61894dfefaf716ffc3
cesaltinofelix <cesaltinofelix2000@gmail.com>
2024-11-26 14:29:54 +0000

    chore: create Makefile for build automation
    - Added a Makefile to automate the compilation and build process.
    - Streamlined the project setup for easier management and execution.

---
+++ process/inter-process-communication/day_1/teste00.c
@@ -0,0 +1,11 @@
+// Exercise: Inter-process Communication with Pipes
+// Write a program that creates a pipe between a parent process and a child process.
+// The parent should send a string to the child, which should convert it to uppercase and send it back to the parent.
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello World");
+    return (0);
+}
